e90208ae392ba119174477db817af82a8374fe71
Fantor2 <113799893+Fantor2@users.noreply.github.com>
2022-11-20 13:22:43 +0300

    Add files via upload

---
+++ 20 11.py
@@ -0,0 +1,101 @@
+#1##import random
+##
+##students_list = ['Гарри Поттер','Гермиона Грейнджер','Драко Малфлой',
+##                 'Рон Уизли','Полумна Лавгул','Чарли Уизли',
+##                 'Джинни Уизли','Фред Уизли','Невилл Догопупс']
+##
+##
+##gryffindor = []
+##slytherin = []
+##hufflepuff = []
+##
+##while len(students_list) !=0:
+##    student = random.choice(students_list)
+##    choice = random.randint(1, 300)
+##    if choice <=100:
+##        gryffindor.append(student)
+##        print(student + "Отпаравляется в Гриффиндор")
+##    elif choice <=200:
+##        slytherin.append(student)
+##        print(student + "Отпаравляется в Слизерин")
+##    else:
+##        hufflepuff.append(student)
+##        print(student + "Отпаравляется в Пуффендуй")
+##    students_list.remove(student)
+##print(students_list,'Студентов без факульта нет')
+##print('В Гриффендоре',gryffindor)
+##print('В Слизерине',slytherin)
+##print('В Пуффендуе',hufflepuff,)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##c = []
+##a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+##b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+##for i in range(len(a)):
+##    constains=0
+##    num=a[0]
+##    for i in range(len(b)):
+##        for num ==b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import random
+a = []
+while len(a) !=10:
+    jo= random.randint(1,100)
+    a.append(jo)
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
